37919005110952634cb71430fab457183a1b5b82
Marta Prieto <g00431347@atu.ie>
2025-09-27 14:00:19 +0100

    Creating project

---
+++ src/main/java/ie/atu/week3cicdpost/Week3CicDpostApplication.java
@@ -0,0 +1,14 @@
+package ie.atu.week3cicdpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week3CicDpostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week3CicDpostApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/ie/atu/week3cicdpost/Week3CicDpostApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week3cicdpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3CicDpostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
